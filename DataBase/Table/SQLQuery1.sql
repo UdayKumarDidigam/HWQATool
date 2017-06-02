a5a20c0c2e20bf9eddadaae48e9029b4e95f065d
pashamd <pashamd.java@gmail.com>
2017-06-02 12:33:59 +0530

    hi

--- DataBase/Table/SQLQuery1.sql
+++ DataBase/Table/SQLQuery1.sql
@@ -2,4 +2,30 @@ create table tblTeam(Id int,Name Varchar(20),QualityBenchMark varchar(20),[Versi
 
 create table tblAuditTimeSlot(Id int,AuditId int,Auditor varchar(20),StartDate date,EndDate date,TotalTime Time,[status] bit,[Version] int,LastModifiedAt date,LastModifiedBy varchar(20));   
 
-create table tblAudit(Id int,AuditNo int,ConfirmationNo int,ServiceRequestNo int,ProcessedDate date,Processor varchar(20),ClientId int,SubTaskId int,TaskId int,Auditor varchar(20),AuditDate date,Comments varchar(20),IsDefect bit,isLearning bit,IsEscalation bit,IsClientFocus bit,IsDuplicate bit,IsSampled bit,NoOfRecords int,TotalDefects int,[Status] varchar(20),PlatformId int,[Version] int,LastModifiedAt date,LastModifiedBy varchar(20));
+CREATE TABLE TBLAUDIT
+(
+[ID] INT,
+[AUDITNO] INT,
+[CONFIRMATIONNO] INT,
+[SERVICEREQUESTNO] INT,
+[PROCESSEDDATE] DATE,[PROCESSOR] VARCHAR(20),
+[CLIENTID] INT,
+[SUBTASKID] INT,
+[TASKID] INT,
+[AUDITOR] VARCHAR(20),
+[AUDITDATE] DATE,
+[COMMENTS] VARCHAR(20),
+[ISDEFECT] BIT,
+[ISLEARNING] BIT,
+[ISESCALATION] BIT,
+[ISCLIENTFOCUS] BIT,
+[ISDUPLICATE] BIT,
+[ISSAMPLED] BIT,
+[NOOFRECORDS] INT,
+[TOTALDEFECTS] INT,
+[STATUS] VARCHAR(20),
+[PLATFORMID] INT,
+[VERSION] INT,
+[LASTMODIFIEDAT] DATE,
+[LASTMODIFIEDBY] VARCHAR(20)
+);
