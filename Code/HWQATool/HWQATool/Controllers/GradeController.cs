eee8b882aefc259ab9a828e333ccdb592336aa49
ASHWAQ423 <ASHWAQ2MUNNA@gmail.com>
2017-06-03 11:05:05 +0530

    Added Code

---
+++ Code/HWQATool/HWQATool/Controllers/GradeController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using HWQATool.Models;
+
+namespace HWQATool.Controllers
+{
+    public class GradeController : Controller
+    {
+        private HWQAToolContext db = new HWQAToolContext();
+
+        // GET: /Grade/
+        public ActionResult Index()
+        {
+            var grades = db.Grades.Include(g => g.Team);
+            return View(grades.ToList());
+        }
+
+        // GET: /Grade/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Grade grade = db.Grades.Find(id);
+            if (grade == null)
+            {
+                return HttpNotFound();
+            }
+            return View(grade);
+        }
+
+        // GET: /Grade/Create
+        public ActionResult Create()
+        {
+            ViewBag.TeamId = new SelectList(db.Teams, "Id", "Name");
+            return View();
+        }
+
+        // POST: /Grade/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="Id,Name,SamplePercentage,TeamId,Version,LastModifiedAt,LastModifiedBy")] Grade grade)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Grades.Add(grade);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.TeamId = new SelectList(db.Teams, "Id", "Name", grade.TeamId);
+            return View(grade);
+        }
+
+        // GET: /Grade/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Grade grade = db.Grades.Find(id);
+            if (grade == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.TeamId = new SelectList(db.Teams, "Id", "Name", grade.TeamId);
+            return View(grade);
+        }
+
+        // POST: /Grade/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="Id,Name,SamplePercentage,TeamId,Version,LastModifiedAt,LastModifiedBy")] Grade grade)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(grade).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.TeamId = new SelectList(db.Teams, "Id", "Name", grade.TeamId);
+            return View(grade);
+        }
+
+        // GET: /Grade/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Grade grade = db.Grades.Find(id);
+            if (grade == null)
+            {
+                return HttpNotFound();
+            }
+            return View(grade);
+        }
+
+        // POST: /Grade/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Grade grade = db.Grades.Find(id);
+            db.Grades.Remove(grade);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Code/HWQATool/HWQATool/Models/HWQAToolContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace HWQATool.Models
+{
+    public class HWQAToolContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public HWQAToolContext() : base("name=HWQAToolContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<HWQATool.Models.Team> Teams { get; set; }
+
+        public System.Data.Entity.DbSet<HWQATool.Models.Grade> Grades { get; set; }
+    
+    }
+}
