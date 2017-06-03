5d3d225b5839f77077e5637b7a32eaf8d55df48c
pashamd <pashamd.java@gmail.com>
2017-06-03 15:08:39 +0530

    Added Audit

---
+++ Code/HWQATool/HWQATool/Controllers/AuditController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using HWQATool.Models;
+
+namespace HWQATool.Controllers
+{
+    public class AuditController : Controller
+    {
+        private HWQAToolContext db = new HWQAToolContext();
+
+        // GET: /Audit/
+        public ActionResult Index()
+        {
+            var audits = db.Audits.Include(a => a.Client).Include(a => a.Platform).Include(a => a.SubTask);
+            return View(audits.ToList());
+        }
+
+        // GET: /Audit/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Audit audit = db.Audits.Find(id);
+            if (audit == null)
+            {
+                return HttpNotFound();
+            }
+            return View(audit);
+        }
+
+        // GET: /Audit/Create
+        public ActionResult Create()
+        {
+            ViewBag.ClientId = new SelectList(db.Clients, "Id", "Name");
+            ViewBag.PlatformId = new SelectList(db.Platforms, "Id", "Name");
+            ViewBag.SubTaskId = new SelectList(db.SubTasks, "Id", "Name");
+            return View();
+        }
+
+        // POST: /Audit/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="Id,AuditNumber,BatchNumber,FileNumber,ServiceRequestNumber,ProcessedDate,Processor,PlatformId,SubTaskId,ClientId,Auditor,AuditDate,AuditorComments,ISDefect,IsLearning,IsEscalation,IsClientFocus,IsDuplicate,IsSampled,NoOfRecords,Status,Version,LastModifiedAt,LastModifiedBy")] Audit audit)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Audits.Add(audit);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ClientId = new SelectList(db.Clients, "Id", "Name", audit.ClientId);
+            ViewBag.PlatformId = new SelectList(db.Platforms, "Id", "Name", audit.PlatformId);
+            ViewBag.SubTaskId = new SelectList(db.SubTasks, "Id", "Name", audit.SubTaskId);
+            return View(audit);
+        }
+
+        // GET: /Audit/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Audit audit = db.Audits.Find(id);
+            if (audit == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ClientId = new SelectList(db.Clients, "Id", "Name", audit.ClientId);
+            ViewBag.PlatformId = new SelectList(db.Platforms, "Id", "Name", audit.PlatformId);
+            ViewBag.SubTaskId = new SelectList(db.SubTasks, "Id", "Name", audit.SubTaskId);
+            return View(audit);
+        }
+
+        // POST: /Audit/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="Id,AuditNumber,BatchNumber,FileNumber,ServiceRequestNumber,ProcessedDate,Processor,PlatformId,SubTaskId,ClientId,Auditor,AuditDate,AuditorComments,ISDefect,IsLearning,IsEscalation,IsClientFocus,IsDuplicate,IsSampled,NoOfRecords,Status,Version,LastModifiedAt,LastModifiedBy")] Audit audit)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(audit).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ClientId = new SelectList(db.Clients, "Id", "Name", audit.ClientId);
+            ViewBag.PlatformId = new SelectList(db.Platforms, "Id", "Name", audit.PlatformId);
+            ViewBag.SubTaskId = new SelectList(db.SubTasks, "Id", "Name", audit.SubTaskId);
+            return View(audit);
+        }
+
+        // GET: /Audit/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Audit audit = db.Audits.Find(id);
+            if (audit == null)
+            {
+                return HttpNotFound();
+            }
+            return View(audit);
+        }
+
+        // POST: /Audit/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Audit audit = db.Audits.Find(id);
+            db.Audits.Remove(audit);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Code/HWQATool/HWQATool/Models/Audit.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace HWQATool.Models
+{
+    [Table("TBL_AUDIT")]
+    public class Audit
+    {
+        [Key]
+        [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("ID")]
+        public int Id { get; set; }
+
+        [Required]
+        [Column("AUDIT_NO")]
+        [StringLength(50)]
+        public string AuditNumber { get; set; }
+
+        [Required]
+        [Column("BATCH_NO")]
+        [StringLength(50)]
+        public string BatchNumber { get; set; }
+
+
+        [Required]
+        [Column("FILE_NO")]
+        [StringLength(50)]
+        public string FileNumber { get; set; }
+
+        [Required]
+        [Column("SERVICE_REQUEST_NO")]
+        [StringLength(50)]
+        public string ServiceRequestNumber { get; set; }
+
+        [Required]
+        [Column("PROCESSED_DATE")]
+        public DateTime ProcessedDate { get; set; }
+
+        [Required]
+        [Column("PROCESSOR")]
+        public int Processor { get; set; }
+
+        [Required]
+        [Column("PLATFORM_ID")]
+        [ForeignKey("Platform")]
+        public int PlatformId { get; set; }
+
+        [Required]
+        [Column("SUB_TASK_ID")]
+        [ForeignKey("SubTask")]
+        public int SubTaskId { get; set; }
+
+        [Required]
+        [Column("CLIENT_ID")]
+        [ForeignKey("Client")]
+        public int ClientId { get; set; }
+
+        [Required]
+        [Column("AUDITOR")]
+        public int Auditor { get; set; }
+
+        [Required]
+        [Column("AUDIT_DATE")]
+        [StringLength(50)]
+        public string AuditDate { get; set; }
+
+        [Required]
+        [Column("AUDITOR_COMMENTS")]
+        [StringLength(50)]
+        public string AuditorComments { get; set; }
+
+        [Required]
+        [Column("IS_DEFECT")]
+        public Boolean ISDefect { get; set; }
+
+        [Required]
+        [Column("IS_LEARNING")]
+        public Boolean IsLearning { get; set; }
+
+
+        [Required]
+        [Column("IS_ESCALATION")]
+        public Boolean IsEscalation { get; set; }
+
+        [Required]
+        [Column("IS_CLIENTFOCUS")]
+        public Boolean IsClientFocus { get; set; }
+
+        [Required]
+        [Column("IS_DUPLICATE")]
+        public Boolean IsDuplicate { get; set; }
+
+        [Required]
+        [Column("IS_SAMPLED")]
+        public Boolean IsSampled { get; set; }
+
+        [Required]
+        [Column("NO_OF_RECORDS")]
+        public int NoOfRecords { get; set; }
+
+        [Required]
+        [Column("STATUS")]
+        public int Status { get; set; }
+
+        [Required]
+        [Column("VERSION")]
+        public int Version { get; set; }
+
+        [Required]
+        [Column("LAST_MODIFIED_AT")]
+        public DateTime LastModifiedAt { get; set; }
+
+        [Required]
+        [Column("LAST_MODIFIED_BY")]
+        [StringLength(50)]
+        public string LastModifiedBy { get; set; }
+
+        public virtual Platform Platform { get; set; }
+        public virtual SubTask SubTask { get; set; }
+        public virtual Client Client { get; set; }
+        public virtual User User { get; set; }
+    }
+}
