eb678d61b68d935e239c311b3cd7c86a3bf7d7de
Safiya4321 <safiya4321@gmail.com>
2017-06-03 13:50:43 +0530

    Added Excel Record Code

---
+++ Code/HWQATool/HWQATool/Controllers/ExcelRecordController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using HWQATool.Models;
+
+namespace HWQATool.Controllers
+{
+    public class ExcelRecordController : Controller
+    {
+        private HWQAToolContext db = new HWQAToolContext();
+
+        // GET: /ExcelRecord/
+        public ActionResult Index()
+        {
+            var excelrecords = db.ExcelRecords.Include(e => e.Team);
+            return View(excelrecords.ToList());
+        }
+
+        // GET: /ExcelRecord/Details/5
+        public ActionResult Details(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ExcelRecord excelrecord = db.ExcelRecords.Find(id);
+            if (excelrecord == null)
+            {
+                return HttpNotFound();
+            }
+            return View(excelrecord);
+        }
+
+        // GET: /ExcelRecord/Create
+        public ActionResult Create()
+        {
+            ViewBag.TeamId = new SelectList(db.Teams, "Id", "Name");
+            return View();
+        }
+
+        // POST: /ExcelRecord/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="Id,TeamId,IsError,ErrorCode,Client,Task,Platform,Processor,ProcessDate,ServiceRequestNo,AuditNo,BatchNo,FileNo,NoOfRecords,Auditor")] ExcelRecord excelrecord)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ExcelRecords.Add(excelrecord);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.TeamId = new SelectList(db.Teams, "Id", "Name", excelrecord.TeamId);
+            return View(excelrecord);
+        }
+
+        // GET: /ExcelRecord/Edit/5
+        public ActionResult Edit(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ExcelRecord excelrecord = db.ExcelRecords.Find(id);
+            if (excelrecord == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.TeamId = new SelectList(db.Teams, "Id", "Name", excelrecord.TeamId);
+            return View(excelrecord);
+        }
+
+        // POST: /ExcelRecord/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="Id,TeamId,IsError,ErrorCode,Client,Task,Platform,Processor,ProcessDate,ServiceRequestNo,AuditNo,BatchNo,FileNo,NoOfRecords,Auditor")] ExcelRecord excelrecord)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(excelrecord).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.TeamId = new SelectList(db.Teams, "Id", "Name", excelrecord.TeamId);
+            return View(excelrecord);
+        }
+
+        // GET: /ExcelRecord/Delete/5
+        public ActionResult Delete(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ExcelRecord excelrecord = db.ExcelRecords.Find(id);
+            if (excelrecord == null)
+            {
+                return HttpNotFound();
+            }
+            return View(excelrecord);
+        }
+
+        // POST: /ExcelRecord/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(long id)
+        {
+            ExcelRecord excelrecord = db.ExcelRecords.Find(id);
+            db.ExcelRecords.Remove(excelrecord);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Code/HWQATool/HWQATool/Models/ExcelRecord.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace HWQATool.Models
+{
+    [Table("TBL_EXCEL_RECORD")]
+    public class ExcelRecord
+    {
+        [Key]
+        [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("ID")]
+        public long Id { get; set; }
+
+        [Required]
+        [Column("TEAM_ID")]
+        [ForeignKey("Team")]
+        public int TeamId { get; set; }
+
+        [Required]
+        [Column("IS_ERROR")]
+        public Boolean IsError { get; set; }
+
+       
+        [Column("ERROR_CODE")]
+        public string ErrorCode { get; set; }
+
+ 
+        [Column("CLIENT")]
+        [StringLength(50)]
+        public string Client {get;set;}
+
+        [Column("TASK")]
+        [StringLength(50)]
+        public string Task {get;set;}
+
+        [Column("PLATFORM")]
+        [StringLength(50)]
+        public string Platform {get;set;}
+
+        [Column("PROCESSOR")]
+        [StringLength(50)]
+        public string Processor {get;set;}
+
+        [Column("PROCESSDATE")]
+        [StringLength(50)]
+        public string ProcessDate {get;set;}
+
+        [Column("SERVICE_REQUEST_NO")]
+        [StringLength(50)]
+        public string ServiceRequestNo {get;set;}
+
+        [Column("AUDIT_NO")]
+        [StringLength(50)]
+        public string AuditNo {get;set;}
+
+        [Column("BATCH_NO")]
+        [StringLength(50)]
+        public string BatchNo {get;set;}
+
+        [Column("FILE_NO")]
+        [StringLength(50)]
+        public string FileNo {get;set;}
+
+        [Column("NO_OF_RECORDS")]
+        [StringLength(50)]
+        public string NoOfRecords {get;set;}
+
+        [Column("AUDITOR")]
+        [StringLength(50)]
+        public string Auditor {get;set;}
+
+        public virtual Team Team { get; set; }
+    
+    }
+}
