7b8e83ec71ced8a2fe317772d65f9d2a94defabe
pashamd <pashamd.java@gmail.com>
2017-06-04 14:36:33 +0530

    Updated

--- Code/HWQATool/HWQATool/Models/HWQAToolContext.cs
+++ Code/HWQATool/HWQATool/Models/HWQAToolContext.cs
@@ -21,21 +21,21 @@ namespace HWQATool.Models
 
         public System.Data.Entity.DbSet<HWQATool.Models.Team> Teams { get; set; }
 
-        public System.Data.Entity.DbSet<HWQATool.Models.Grade> Grades { get; set; }
+        //public System.Data.Entity.DbSet<HWQATool.Models.Grade> Grades { get; set; }
 
-        public System.Data.Entity.DbSet<HWQATool.Models.Function> Functions { get; set; }
+        //public System.Data.Entity.DbSet<HWQATool.Models.Function> Functions { get; set; }
 
-        public System.Data.Entity.DbSet<HWQATool.Models.Error> Errors { get; set; }
-		public System.Data.Entity.DbSet<HWQATool.Models.SubTask> SubTasks { get; set; }
-        public System.Data.Entity.DbSet<HWQATool.Models.User> Users { get; set; }
+        //public System.Data.Entity.DbSet<HWQATool.Models.Error> Errors { get; set; }
+        //public System.Data.Entity.DbSet<HWQATool.Models.SubTask> SubTasks { get; set; }
+        //public System.Data.Entity.DbSet<HWQATool.Models.User> Users { get; set; }
 
-        public System.Data.Entity.DbSet<HWQATool.Models.UserRole> UserRoles { get; set; }
+        //public System.Data.Entity.DbSet<HWQATool.Models.UserRole> UserRoles { get; set; }
 
-        public System.Data.Entity.DbSet<HWQATool.Models.Audit> Audits { get; set; }
+        //public System.Data.Entity.DbSet<HWQATool.Models.Audit> Audits { get; set; }
 
-        public System.Data.Entity.DbSet<HWQATool.Models.Client> Clients { get; set; }
+        //public System.Data.Entity.DbSet<HWQATool.Models.Client> Clients { get; set; }
 
-        public System.Data.Entity.DbSet<HWQATool.Models.Platform> Platforms { get; set; }
+        //public System.Data.Entity.DbSet<HWQATool.Models.Platform> Platforms { get; set; }
 
         public System.Data.Entity.DbSet<HWQATool.Models.ExcelRecord> ExcelRecords { get; set; }
     
