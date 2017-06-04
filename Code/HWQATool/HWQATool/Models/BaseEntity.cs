14d812d727d8bba18ecfc98c619bb1c0d871a11e
ASHWAQ423 <ASHWAQ2MUNNA@gmail.com>
2017-06-04 10:27:38 +0530

    Added BaseEntity

---
+++ Code/HWQATool/HWQATool/Models/BaseEntity.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace HWQATool.Models
+{
+    public class BaseEntity<T>
+    {
+        [Key]
+        [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("ID")]
+        public T Id { get; set; }
+
+        [Required]
+        [Column("IS_ACTIVE")]
+        [Display(Name = "Active")]
+        public bool IsActive { get; set; }
+
+        [Required]
+        [Column("VERSION")]
+        public int Version { get; set; }
+
+        [Required]
+        [Column("LAST_MODIFIED_AT")]
+        [Display(Name = "Last Modified At")]
+        public DateTime LastModifiedAt { get; set; }
+
+        [Required]
+        [Column("LAST_MODIFIED_BY", TypeName = "VARCHAR")]
+        [StringLength(50)]
+        [Display(Name = "Last Modified By")]
+        public string LastModifiedBy { get; set; }
+    }
+}
--- Code/HWQATool/HWQATool/Models/Team.cs
+++ Code/HWQATool/HWQATool/Models/Team.cs
@@ -8,16 +8,10 @@ using System.Web;
 namespace HWQATool.Models
 {
     [Table("TBL_TEAM")]
-    public class Team
+    public class Team : BaseEntity <int>
     {
-        [Key]
         [Required]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        [Column("ID")]
-        public int Id { get; set; }
-
-        [Required]
-        [Column("NAME")]
+        [Column("NAME", TypeName = "VARCHAR")]
         [StringLength(50)]
         [Index(IsUnique = true)]
         public string Name { get; set; }
@@ -27,18 +21,5 @@ namespace HWQATool.Models
         [DisplayFormat(DataFormatString = "{0:P2}", ApplyFormatInEditMode = true)]
         [Display(Name = "Quality Bench Mark")]
         public decimal QualityBenchMark { get; set; }
-
-        [Required]
-        [Column("VERSION")]
-        public int Version { get; set; }
-
-        [Required]
-        [Column("LAST_MODIFIED_AT")]
-        public DateTime LastModifiedAt { get; set; }
-
-        [Required]
-        [Column("LAST_MODIFIED_BY")]
-        [StringLength(50)]
-        public string LastModifiedBy { get; set; }
     }
 }
