c1ed5974d7fdf273412ac92e474f558c2390a16b
Safiya4321 <safiya4321@gmail.com>
2017-06-04 11:28:56 +0530

    Added excel controller

---
+++ Code/HWQATool/HWQATool/Controllers/UserRoleController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HWQATool.Models
+{
+    public class UserRoleController : Controller
+    {
+        private HWQAToolContext db = new HWQAToolContext();
+
+        // GET: /UserRole/
+        public ActionResult Index()
+        {
+            var userroles = db.UserRoles.Include(u => u.User);
+            return View(userroles.ToList());
+        }
+
+        // GET: /UserRole/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserRole userrole = db.UserRoles.Find(id);
+            if (userrole == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userrole);
+        }
+
+        // GET: /UserRole/Create
+        public ActionResult Create()
+        {
+            ViewBag.UserId = new SelectList(db.Users, "Id", "Email");
+            return View();
+        }
+
+        // POST: /UserRole/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="Id,UserId,Role,Version,LastModifiedAt,LastModifiedBy")] UserRole userrole)
+        {
+            if (ModelState.IsValid)
+            {
+                db.UserRoles.Add(userrole);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.UserId = new SelectList(db.Users, "Id", "Email", userrole.UserId);
+            return View(userrole);
+        }
+
+        // GET: /UserRole/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserRole userrole = db.UserRoles.Find(id);
+            if (userrole == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.UserId = new SelectList(db.Users, "Id", "Email", userrole.UserId);
+            return View(userrole);
+        }
+
+        // POST: /UserRole/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="Id,UserId,Role,Version,LastModifiedAt,LastModifiedBy")] UserRole userrole)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(userrole).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.UserId = new SelectList(db.Users, "Id", "Email", userrole.UserId);
+            return View(userrole);
+        }
+
+        // GET: /UserRole/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserRole userrole = db.UserRoles.Find(id);
+            if (userrole == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userrole);
+        }
+
+        // POST: /UserRole/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            UserRole userrole = db.UserRoles.Find(id);
+            db.UserRoles.Remove(userrole);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
