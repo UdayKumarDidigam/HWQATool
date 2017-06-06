765b67b176656a87ebcf5c59f74f2d194bfc3b6f
UdayKumarDidigam <udaydidigam@hotmail.com>
2017-06-06 17:25:39 +0530

    Update SubTask.cs

--- Code/HWQATool/HWQATool/Models/SubTask.cs
+++ Code/HWQATool/HWQATool/Models/SubTask.cs
@@ -1,47 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Web;
 
 namespace HWQATool.Models
 {
     [Table("TBL_SUB_TASK")]
-    public class SubTask
+    public class SubTask : BaseEntity<int>
     {
-
-        [Key]
-        [Required]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        [Column("ID")]
-        public int Id { get; set; }
-
-        [Required]
-        [Column("Name", TypeName = "VARCHAR")]
-        [Index("UQ_TBL_SUB_TASK_TASK_ID_NAME", 1, IsUnique = true)]
-        [StringLength(50)]
-        public string Name { get; set; }
-
         [Required]
         [Column("TASK_ID")]
-        [Index("UQ_TBL_SUB_TASK_TASK_ID_NAME", 2, IsUnique = true)]
+        [Index("UQ_TBL_SUB_TASK_TASK_ID_NAME", 1, IsUnique = true)]
         [ForeignKey("Task")]
         public int TaskId { get; set; }
 
         [Required]
-        [Column("VERSION")]
-        public int Version { get; set; }
-
-        [Required]
-        [Column("LAST_MODIFIED_AT")]
-        public DateTime LastModifiedAt { get; set; }
-
-        [Required]
-        [Column("LAST_MODIFIED_BY", TypeName = "VARCHAR")]
+        [Column("Name", TypeName = "VARCHAR")]
+        [Index("UQ_TBL_SUB_TASK_TASK_ID_NAME", 2, IsUnique = true)]
         [StringLength(50)]
-        public string LastModifiedBy { get; set; }
+        public string Name { get; set; }
 
-        public virtual Function Task { get; set; }
+        public virtual Function Function { get; set; }
     }
 }
