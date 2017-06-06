192e70bbbef3aa743f8c4a0052eb6fa176093591
UdayKumarDidigam <udaydidigam@hotmail.com>
2017-06-06 17:13:06 +0530

    Update Client.cs

--- Code/HWQATool/HWQATool/Models/Client.cs
+++ Code/HWQATool/HWQATool/Models/Client.cs
@@ -1,61 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Web;
 
 namespace HWQATool.Models
 {
     [Table("TBL_CLIENT")]
-    public class Client
+    public class Client : BaseEntity<int>
     {
-        [Key]
-        [Required]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        [Column("ID")]
-        public int Id { get; set; }
-
-        [Required]
-        [Column("NAME")]
-        [Index("UQ_TBL_CLIENT_TEAM_ID_NAME", 1, IsUnique = true)]
-        [StringLength(50)]
-        [Index(IsUnique = true)]
-        public string Name { get; set; }
-
         [ForeignKey("Team")]
         [Required]
-        [Index("UQ_TBL_CLIENT_TEAM_ID_NAME", 2, IsUnique = true)]
+        [Index("UQ_TBL_CLIENT_TEAM_ID_NAME", 1, IsUnique = true)]
         [Column("TEAM_ID")]
         public int TeamId { get; set; }
 
         [Required]
-        [Column("IS_KEY_CLIENT")]
-        public Boolean Iskeyclient { get; set; }
+        [Column("NAME")]
+        [Index("UQ_TBL_CLIENT_TEAM_ID_NAME", 2, IsUnique = true)]
+        [StringLength(50)]
+        [Index(IsUnique = true)]
+        public string Name { get; set; }
 
+        [Required]
+        [Column("IS_KEY_CLIENT")]
+        [Display(Name = "Key Client")]
+        public bool IsKeyClient { get; set; }
 
         [Required]
         [Column("SAMPLE_PERCENTAGE")]
-        [DisplayFormat(DataFormatString = "{0:P2}", ApplyFormatInEditMode = true)]
+        [DisplayFormat(DataFormatString = "{0:N2}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Sample Percentage")]
         public decimal SamplePercentage { get; set; }
-
-        [Required]
-        [Column("IS_ACTIVE")]
-        public Boolean Isactive { get; set; }
-
-        [Required]
-        [Column("VERSION")]
-        public int Version { get; set; }
-
-
-        [Required]
-        [Column("LAST_MODIFIED_AT")]
-        public DateTime LastModifiedAt { get; set; }
-
-        [Required]
-        [Column("LAST_MODIFIED_BY")]
-        [StringLength(50)]
-        public string LastModifiedBy { get; set; }
 
         public virtual Team Team{ get; set; }
     }
