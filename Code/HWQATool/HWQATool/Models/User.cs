8d73fcef6b7d1089647fd0862e23cdac17c2d788
UdayKumarDidigam <udaydidigam@hotmail.com>
2017-06-06 17:35:48 +0530

    Update User.cs

--- Code/HWQATool/HWQATool/Models/User.cs
+++ Code/HWQATool/HWQATool/Models/User.cs
@@ -1,38 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Web;
 
 namespace HWQATool.Models
 {
     [Table("TBL_USER")]
-    public class User
+    public class User : BaseEntity<int>
     {
-        [Key]
         [Required]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        [Column("ID")]
-        public int Id { get; set; }
+        [Column("LOGIN_ID", TypeName = "VARCHAR")]
+        [Index("UQ_TBL_USER_LOGIN_ID", IsUnique = true)]
+        [StringLength(20)]
+
+        public int LoginId { get; set; }
 
         [Required]
-        [Column("EMAIL",TypeName = "VARCHAR")]
-        [Index("UQ_TBL_USER_EMAIL", 1, IsUnique = true)]
+        [Column("ASSOCIATE_ID", TypeName = "VARCHAR")]
+        [Index("UQ_TBL_USER_ASSOCIATE_ID", IsUnique = true)]
+        [StringLength(10)]
+        public int AssociateId { get; set; }
+
+        [Required]
+        [Column("EMAIL", TypeName = "VARCHAR")]
+        [Index("UQ_TBL_USER_EMAIL", IsUnique = true)]
         [StringLength(200)]
         public string Email { get; set; }
-
-        [Required]
-        [Column("LOGIN_ID")]
-        [Index("UQ_TBL_USER_Login_Id", 1, IsUnique = true)]
-        
-        public int LoginId { get; set; }
 
         [Required]
         [Column("FIRSTNAME", TypeName = "VARCHAR")]
         [StringLength(50)]
         public string FirstName { get; set; }
-
 
         [Required]
         [Column("LASTNAME", TypeName = "VARCHAR")]
@@ -46,16 +42,17 @@ namespace HWQATool.Models
 
         [Column("SUPERVOISER_EMAIL", TypeName = "VARCHAR")]
         [StringLength(200)]
+        [Display(Name = "Supervoiser Email")]
         public string Supervoiser_Email { get; set; }
 
         [Required]
         [Column("GRADE_ID")]
         [ForeignKey("Grade")]
-        public int GradeId{ get; set; }
+        public int GradeId { get; set; }
 
         [Required]
         [Column("SAMPLE_PERCENTAGE")]
-        [DisplayFormat(DataFormatString = "{0:P2}", ApplyFormatInEditMode = true)]
+        [DisplayFormat(DataFormatString = "{0:N2}", ApplyFormatInEditMode = true)]
         [Display(Name = "Sample Percentage")]
         public decimal SamplePercentage { get; set; }
 
@@ -63,19 +60,6 @@ namespace HWQATool.Models
         [StringLength(50)]
         public string Comments { get; set; }
 
-        [Required]
-        [Column("VERSION")]
-        public int Version { get; set; }
-
-        [Required]
-        [Column("LAST_MODIFIED_AT")]
-        public DateTime LastModifiedAt { get; set; }
-
-        [Required]
-        [Column("LAST_MODIFIED_BY")]
-        [StringLength(50)]
-        public string LastModifiedBy { get; set; }
-
-        public virtual  Grade Grade { get; set; }
+        public virtual Grade Grade { get; set; }
     }
 }
