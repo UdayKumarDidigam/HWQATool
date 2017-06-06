e857972d7661d9c50c700cde17f491e23a9c51b9
UdayKumarDidigam <udaydidigam@hotmail.com>
2017-06-06 18:17:39 +0530

    Update AuditFinding.cs

--- Code/HWQATool/HWQATool/Models/AuditFinding.cs
+++ Code/HWQATool/HWQATool/Models/AuditFinding.cs
@@ -1,48 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Web;
 
 namespace HWQATool.Models
 {
     [Table("TBL_AUDIT_FINDING")]
-    public class AuditFinding
+    public class AuditFinding : BaseEntity<long>
     {
-        
-        [Key]
-        [Required]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        [Column("ID")]
-        public int Id { get; set; }
-
-        [ForeignKey("Error")]
         [Required]
         [Index("UQ_TBL_AUDIT_FINDING_AUDIT_ID_ERROR_ID", 1, IsUnique = true)]
         [Column("AUDIT_ID")]
-        public int Auditid { get; set; }
-
-        [ForeignKey("Error")]
+        [ForeignKey("Audit")]
+        public long AuditId { get; set; }
+        
         [Required]
         [Index("UQ_TBL_AUDIT_FINDING_AUDIT_ID_ERROR_ID", 2, IsUnique = true)]
         [Column("ERROR_ID")]
+        [ForeignKey("Error")]
         public int Errorid { get; set; }
 
-        [Required]
-        [Column("VERSION")]
-        public int Version { get; set; }
-
-        [Required]
-        [Column("LAST_MODIFIED_AT")]
-        public DateTime LastModifiedAt { get; set; }
-
-        [Required]
-        [Column("LAST_MODIFIED_BY")]
-        [StringLength(50)]
-        public string LastModifiedBy { get; set; }
-
-        public virtual Error error { get; set; }
+        public virtual Audit Audit { get; set; }
+        public virtual Error Error { get; set; }
 
     }
 }
