3b12c26e0ea44e4ebd9dc4d8b855cea3d2fba0db
UdayKumarDidigam <udaydidigam@hotmail.com>
2017-06-06 19:24:17 +0530

    Update SubTask.cs

--- Code/HWQATool/HWQATool/Models/SubTask.cs
+++ Code/HWQATool/HWQATool/Models/SubTask.cs
@@ -18,6 +18,6 @@ namespace HWQATool.Models
         [StringLength(50)]
         public string Name { get; set; }
 
-        public virtual Function Function { get; set; }
+        public virtual Task Task { get; set; }
     }
 }
