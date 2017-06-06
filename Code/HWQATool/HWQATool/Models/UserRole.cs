15fdd384a829700cc4c47724adfa352a5ba3a985
UdayKumarDidigam <udaydidigam@hotmail.com>
2017-06-06 17:39:13 +0530

    Update UserRole.cs

--- Code/HWQATool/HWQATool/Models/UserRole.cs
+++ Code/HWQATool/HWQATool/Models/UserRole.cs
@@ -1,46 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Web;
 
 namespace HWQATool.Models
 {
     [Table("TBL_USER_ROLE")]
-    public class UserRole
+    public class UserRole : BaseEntity<int>
     {
-        [Key]
-        [Required]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        [Column("ID")]
-        public int Id { get; set; }
-
         [Required]
         [Column("USER_ID")]
-        [Index("UQ_TBL_USER_ROLE_User_Id", 1, IsUnique = true)]
+        [Index("UQ_TBL_USER_ROLE_USER_ID_ROLE", 1, IsUnique = true)]
         [ForeignKey("User")]
-
         public int UserId { get; set; }
 
         [Required]
         [Column("ROLE")]
-        [Index("UQ_TBL_USER_ROLE_User_Id", 2, IsUnique = true)]        
-        public int Role { get; set; }
-
-        [Required]
-        [Column("VERSION")]
-        public int Version { get; set; }
-
-        [Required]
-        [Column("LAST_MODIFIED_AT")]
-        public DateTime LastModifiedAt { get; set; }
-
-        [Required]
-        [Column("LAST_MODIFIED_BY")]
-        [StringLength(50)]
-        public string LastModifiedBy { get; set; }
+        [Index("UQ_TBL_USER_ROLE_USER_ID_ROLE", 2, IsUnique = true)]
+        public Role Role { get; set; }
 
         public virtual User User { get; set; }
+    }
+
+    public enum Role
+    {
+        Administrator = 1,
+        Supervisor = 2,
+        Auditor = 3,
+        Processor = 4,
+        Reporter = 5
     }
 }
