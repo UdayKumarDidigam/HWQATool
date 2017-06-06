db2a092d812c3a6c37e9179305438a6b8ed7140e
UdayKumarDidigam <udaydidigam@hotmail.com>
2017-06-06 17:14:57 +0530

    Update Grade.cs

--- Code/HWQATool/HWQATool/Models/Grade.cs
+++ Code/HWQATool/HWQATool/Models/Grade.cs
@@ -1,54 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Web;
 
 namespace HWQATool.Models
 {
-    [Table("TBL_TGRADE")]
-    public class Grade
+    [Table("TBL_GRADE")]
+    public class Grade : BaseEntity<int>
     {
-        [Key]
-        [Required]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        [Column("ID")]
-        public int Id { get; set; }
-
-        [Required]
-        [Column("NAME")]
-        [StringLength(50)]
-        [Index("UQ_TBL_TGRADE_TEAM_ID_NAME", 1, IsUnique = true)]
-        public string Name { get; set; }
-
-        [Required]
-        [Column("SAMPLE_PERCENTAGE")]
-        [DisplayFormat(DataFormatString = "{0:P2}", ApplyFormatInEditMode = true)]
-        [Display(Name = "Sample Percentage")]
-        public decimal SamplePercentage { get; set; }
-
         [Required]
         [Column("TEAM_ID")]
-        [Index("UQ_TBL_TGRADE_TEAM_ID_NAME", 2, IsUnique = true)]
+        [Index("UQ_TBL_GRADE_TEAM_ID_NAME", 1, IsUnique = true)]
         [ForeignKey("Team")]
         public int TeamId { get; set; }
 
         [Required]
-        [Column("VERSION")]
-        public int Version { get; set; }
-
-        [Required]
-        [Column("LAST_MODIFIED_AT")]
-        public DateTime LastModifiedAt { get; set; }
-
-        [Required]
-        [Column("LAST_MODIFIED_BY")]
+        [Column("Name", TypeName = "VARCHAR")]
+        [Index("UQ_TBL_GRADE_TEAM_ID_NAME", 2, IsUnique = true)]
         [StringLength(50)]
-        public string LastModifiedBy { get; set; }
+        public string Name { get; set; }
 
+        [Required]
+        [Column("SAMPLE_PERCENTAGE")]
+        [DisplayFormat(DataFormatString = "{0:N2}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Sample Percentage")]
+        public decimal SamplePercentage { get; set; }
 
         public virtual Team Team { get; set; }
-
     }
 }
